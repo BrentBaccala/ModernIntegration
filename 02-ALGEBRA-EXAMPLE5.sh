d3ae8e417541aedd7c3146026e9ce26d73115521
Brent Baccala,,, <baccala@freesoft.org>
2007-01-26 21:55:03 +0000

    Initial revision

---
+++ 02-ALGEBRA-EXAMPLE5.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#./long-division.pl '2x^2-(y)x-(y^2)' '(5y-7)x-(5y^2-7y)'
+./long-division.pl '2x^2-(y)x-(y^2)' 'x-(y)'
---
+++ 02-ALGEBRA-EXAMPLE5a.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+./long-division.pl '-y^2-(x)y+(2x^2)' '-5y^2+(5x+7)y-(7x)'
+echo "\\bigskip"
+./long-division.pl '-5y^2+(5x+7)y-(7x)' '-(2x+7/5)y+(2x^2+7/5x)'
